d9d326fe57ea84230a7d1a5ba71998a561439649
Iftimis Cosmin <iftimia.cosmin@yahoo.com>
2025-03-20 19:26:39 +0200

    Integratate Spring boot

---
+++ src/main/java/com/cosmin/itschool/ItSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.cosmin.itschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItSchoolApplication.class, args);
+	}
+
+}
